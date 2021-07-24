d2e28c568936291b8cec70ed64c6eef16d1f436a
Davis Chang <davis.chang@langlive.com>
2021-07-24 16:08:16 +0800

    init version

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div onClick={e => alert('Hello World!!')}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
