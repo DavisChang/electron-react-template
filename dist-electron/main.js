010d851325ac6482586e5eac119b4a24b607ab1f
Davis Chang <davischang1030@gmail.com>
2024-10-27 16:08:31 +0800

    layout

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,10 +1,9 @@
 import { app, BrowserWindow } from "electron";
-import { createRequire } from "node:module";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
-createRequire(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 process.env.APP_ROOT = path.join(__dirname, "..");
+const MAIN_VITE_OPEN_DEV_TOOLS = process.env.MAIN_VITE_OPEN_DEV_TOOLS || false;
 const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
 const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
 const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
@@ -12,10 +11,13 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 
 let win;
 function createWindow() {
   win = new BrowserWindow({
+    title: "This is vite application",
     icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.mjs")
     }
+    // fullscreen: true,
   });
   win.webContents.on("did-finish-load", () => {
     win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
@@ -24,6 +26,9 @@ function createWindow() {
     win.loadURL(VITE_DEV_SERVER_URL);
   } else {
     win.loadFile(path.join(RENDERER_DIST, "index.html"));
+  }
+  if (MAIN_VITE_OPEN_DEV_TOOLS) {
+    win.webContents.openDevTools();
   }
 }
 app.on("window-all-closed", () => {
@@ -40,6 +45,7 @@ app.on("activate", () => {
 app.whenReady().then(createWindow);
 export {
   MAIN_DIST,
+  MAIN_VITE_OPEN_DEV_TOOLS,
   RENDERER_DIST,
   VITE_DEV_SERVER_URL
 };
--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,41 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/electron-vite.animate.svg";
 import Btn from "components/Btn.tsx";
-import "./App.css";
+import { RootLayout, Sidebar, Content } from "@/components/AppLayout";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <RootLayout>
+      <Sidebar className="p-2 border-4 border-red-500">Sidebar</Sidebar>
+      <Content className="border-4 border-blue-500">
+        <div>
+          <div className="flex justify-center m-4">
+            <a href="https://electron-vite.github.io" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
 
-      <h1 className="text-3xl font-bold underline">
-        Electron with Vite + React
-      </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <Btn />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+          <h1 className="text-3xl font-bold underline">
+            Electron with Vite + React
+          </h1>
+          <div>
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+            <Btn />
+          </div>
+          <p>Click on the Vite and React logos to learn more</p>
+        </div>
+      </Content>
+    </RootLayout>
   );
 }
 
